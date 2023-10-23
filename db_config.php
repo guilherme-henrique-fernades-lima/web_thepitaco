560af83156125def195de70718f28c3995994a55
Cairo <cairofelipedev@gmail.com>
2023-10-23 12:07:04 -0300

    clear repo

--- db_config.php
+++ db_config.php
@@ -1,3 +1,3 @@
 <?php
 // Conectar ao banco de dados usando a classe PDO
-$pdo = new PDO('mysql:host=localhost;dbname=thepitaco', 'root', '');
+$pdo = new PDO('mysql:host=localhost;dbname=thepitaco', 'root', 'root');
