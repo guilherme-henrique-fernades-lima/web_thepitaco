4db94f6dee210c2521d42a2621203f073cc5039c
WallaceNeryVeras <wallacenery.wn@gmail.com>
2023-10-24 14:49:19 -0300

    working layout blogs

--- components/blogs.php
+++ components/blogs.php
@@ -17,3 +17,23 @@
     ?>
   </div>
 </div>
+
+<div class="mx-auto max-w-7xl px-2 pt-4">
+  <h1 class="text-color1 text-center text-3xl font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-left md:text-3xl md:leading-14">
+    Notícias
+  </h1>
+
+  <div class="grid lg:grid-cols-3 pt-4">
+    <?php foreach ($blogs as $blog) { ?>
+      <div class="mt-4 px-2">
+        <div class="divblogs h-4/5 w-full scale-100">
+          <h1 class="text-color1 title-font mb-3 text-lg font-medium"><?php echo $blog['name']; ?></h1>
+          <img class='lazy rounded-md h-52 w-full' src='./admin/uploads/blogs/<?php echo $blog['img'] ?>'>
+          <p class="text-color1 mb-3 text-md">
+            <?php echo substr($blog['description'], 219, 140); ?>
+          </p>
+        </div>
+      </div>
+    <?php } ?>
+  </div>
+</div>
