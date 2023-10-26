28d939d90e7435c7bc6fa7e94d7ef356f9e41214
WallaceNeryVeras <wallacenery.wn@gmail.com>
2023-10-26 19:12:08 -0300

    adjustment

--- components/blogs.php
+++ components/blogs.php
@@ -7,7 +7,7 @@
     <?php foreach ($blogs as $blog) { ?>
       <div class="mt-4 px-2">
         <a class="flex items-center h-56 max-w-4xl border border-transparent hover:border-color2 border-2 rounded-md px-2" href="<?php echo $URI->base('blog/' . slugify($blog['name'])); ?>">
-          <img class='lazy rounded-md h-48 cover' src='./admin/uploads/blogs/<?php echo $blog['img'] ?>'>
+          <img class='lazy rounded-lg h-48 cover' src='./admin/uploads/blogs/<?php echo $blog['img'] ?>'>
           <div class="ml-4">
             <h1 class="text-color2 title-font text-lg font-semibold font-sans"><?php echo $blog['name']; ?></h1>
             <p class="font-sans mt-2">
